2be38c0b20009917a26510ade55bca5348702290
Jeremy Swinarton <jeremy@swinarton.com>
2022-07-02 14:14:17 -0400

    Hex printing!!!

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ PROGS = $(patsubst boot_%.asm, boot_%.bin, $(SRCS))
 
 all: $(PROGS)
 
+boot_string.bin: boot_string.asm print.asm
+	nasm boot_string.asm -f bin -o boot_string.bin
+
 boot_%.bin: boot_%.asm
 	nasm $< -f bin -o $@
 
--- boot_string.asm
+++ boot_string.asm
@@ -6,6 +6,18 @@ call print_string
 mov bx, GOODBYE_MSG
 call print_string
 
+mov dx, 0x1fb6
+call print_hex
+
+mov dx, 0xabcd
+call print_hex
+
+mov dx, 0x00ff
+call print_hex
+
+mov dx, 0x7c00
+call print_hex
+
 jmp $
 
 %include "print.asm"
